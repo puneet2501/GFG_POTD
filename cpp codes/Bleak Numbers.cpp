c71b805cd6eb705d01ccdfb4c9ce5b9e1f893797
Puneet <puneet19081@iiitd.ac.in>
2023-10-28 10:52:39 +0530

    Bleak Numbers

---
+++ cpp codes/Bleak Numbers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int is_bleak(int n) {
+        int m = n;
+        int x = 0;
+        while (n > 0) {
+            n = n / 2;
+            x++;
+        }
+
+        for (int i = m - 1; i >= m - x; i--) {
+            if (i + countSetBits(i) == m)
+                return 0;
+        }
+
+        return 1;
+    }
+
+    int countSetBits(int x) {
+        int ans = 0;
+        while (x > 0) {
+            if ((x & 1) == 1)
+                ans++;
+            x = x >> 1;
+        }
+        return ans;
+    }
+};
