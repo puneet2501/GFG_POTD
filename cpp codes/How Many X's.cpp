2f0e411d6664e4b0dd133158ffc9d86f52a44b4d
Puneet <puneet19081@iiitd.ac.in>
2023-12-06 00:36:59 +0530

    How Many X's?

---
+++ cpp codes/How Many X's.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int countX(int L, int R, int X) {
+        // code here
+        int count =0;
+        for(int i =L+1;i<R ;i++){
+            int num=i;
+            while(num!=0){
+                if(num%10==X)count++;
+                num=num/10;
+           }
+        }
+        return count;
+    }
+};
