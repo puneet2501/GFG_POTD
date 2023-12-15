7d76628d657cccbbca58f6300685f6c6425a62c3
Puneet <puneet19081@iiitd.ac.in>
2023-12-16 01:43:27 +0530

    String's count

---
+++ cpp codes/String's Count.cpp
@@ -0,0 +1,12 @@
+long long int countStr(long long int n){
+
+	    // all a       --> 1
+	    // 1b and a    --> n
+	    // 1c and a    --> n
+	    // 2c and a    --> n*(n-1)/2
+	    // 1b,1c and a --> n*(n-1)
+	    // 1b,2c and a --> n(n-1)(n-2)/2
+	    
+	    
+	    return  1+ 2*n + n*(n-1)/2 + n*(n-1) + n*(n-1)*(n-2)/2;
+}
