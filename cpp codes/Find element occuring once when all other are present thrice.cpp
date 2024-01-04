c6de5982f1415355102a79a7060537cf85388075
Puneet <puneet19081@iiitd.ac.in>
2024-01-04 08:20:57 +0530

    Find element occuring once when all other are present thrice

---
+++ cpp codes/Find element occuring once when all other are present thrice.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int singleElement(int arr[] ,int N) {
+        // code here
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            int setbit = 0;
+            for (int j = 0; j < N; j++) {
+                if ((arr[j] & (1 << i)) != 0) {
+                    setbit++;
+                }
+            }
+            if (setbit % 3 == 1) {
+                res = res | (1 << i);
+            }
+        }
+        return res;
+    }
+};
