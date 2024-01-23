e9846e3d68ccf2d56e122c5ebefa2bb967f1669f
Puneet <puneet19081@iiitd.ac.in>
2024-01-23 07:50:13 +0530

    Course Schedule

---
+++ cpp codes/Course Schedule.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+  public:
+    vector<int> findOrder(int n, int m, vector<vector<int>> prerequisites) 
+    {
+        vector<int>adj[n];
+        for(auto it:prerequisites)
+        {
+            adj[it[1]].push_back(it[0]);
+        }
+        
+        int indegree[n]={0};
+        for(int i = 0; i < n; i++)
+        {
+            for(auto it:adj[i])
+                indegree[it]++;
+        }
+        
+        queue<int>q;
+        for(int i = 0; i < n; i++)
+        {
+            if(indegree[i] == 0)
+                q.push(i);
+        }
+        
+        vector<int>topo;
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            
+            for(auto it:adj[node])
+            {
+                indegree[it]--;
+                if(indegree[it] == 0)
+                    q.push(it);
+            }
+        }
+        
+        if(topo.size() == n) return topo;
+        return {};
+    }
+};
